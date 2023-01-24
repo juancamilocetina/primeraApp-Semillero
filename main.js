1207938179366e3188ea3546bccd17b83a501219
Juan Cetina <juan.cetina@correo.uis.edu.co>
2023-01-24 17:44:35 -0500

    Logica edad

--- main.js
+++ main.js
@@ -3,27 +3,17 @@ var app = new Vue({
     data: {
       message: 'Hello Vue!!!!!!!!!!!',
       n1 : 0,
-      n2 : 0,
-      n3 : 0,
-      n4 : 0,
-      rta : "",
-      arrs : [1,2,3,4,5]
     },
     methods: {
-        suma(){
-            this.rta = this.n1 + this.n2
-        },
-        resta(){
-            this.rta = this.n1 - this.n2
-        },
-        multiplicacion(){
-            this.rta = this.n1 * this.n2
-        },
-        division(){
-            this.rta = this.n1 / this.n2
-        },
-        mm(){
-            this.rta = this.n1 / this.n2
+        edad(){
+            const fechaNacimiento = document.getElementById("fechaNacimiento");
+            console.log(fechaNacimiento)
+            window.addEventListener('load', function () {
+
+                fechaNacimiento.addEventListener('change', function () {
+                    console.log(this.value);
+                });
+            });
         }
     }
   })
